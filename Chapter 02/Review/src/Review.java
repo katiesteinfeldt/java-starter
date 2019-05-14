14739564e604338cd34f108efe8ea47837da81e4
Katie Mangan <katie.mangan1@gmail.com>
2019-05-14 17:05:24 -0500

    initial commit

---
+++ Chapter 02/Review/src/Review.java
@@ -0,0 +1,12 @@
+
+public class Review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "Katie";
+		float favoriteNumber = 13;
+		System.out.println(myName + "'s favorite number is " + favoriteNumber);
+
+	}
+
+}
